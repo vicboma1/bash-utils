3f01ddabdda9a76c4705c95b09bcae6fac3373c7
Victor Bolinches <vicboma1@users.noreply.github.com>
2021-06-08 20:01:11 +0200

    Update anyadir-bat-PCLauncherIni.sh

--- anyadir-bat-PCLauncherIni.sh
+++ anyadir-bat-PCLauncherIni.sh
@@ -4,7 +4,9 @@ for file in $(find . -name "*.iso*" -type f | sed 's/ /_/g')
 do       
  ori=$(echo "${file}" | sed 's/_/ /g')
  filename="${ori%.*}"
- echo "[${ori#*./}]" >> "E:\Hyperspin-1.5\RocketLauncher\Modules\PCLauncher\PCLauncher.ini"
- echo "FadeTitle=Dolphin" >> "E:\Hyperspin-1.5\RocketLauncher\Modules\PCLauncher\PCLauncher.ini"
- echo "Application=..\..\_Arcade\the-eye.eu\public\rom\Nintendo GameCube\Dolphin-x64\${filename#*./}.bat">> "E:\Hyperspin-1.5\RocketLauncher\Modules\PCLauncher\PCLauncher.ini"
+ name=${filename#*./}
+ _bat=$(echo "${name}.bat")
+ echo "[${name}]"   >> "E:\Hyperspin-1.5\RocketLauncher\Modules\PCLauncher\PCLauncher.ini"
+ echo "FadeTitle=Dolphin"   >> "E:\Hyperspin-1.5\RocketLauncher\Modules\PCLauncher\PCLauncher.ini"
+ echo "Application=..\..\_Arcade\the-eye.eu\public\rom\Nintendo GameCube\Dolphin-x64\\${_bat}"  >> "E:\Hyperspin-1.5\RocketLauncher\Modules\PCLauncher\PCLauncher.ini"
 done
